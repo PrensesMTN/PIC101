a5fef402fce5be4f3829ba05561c986a76a80108
PrensesMTN <prensesmtn@gmail.com>
2023-08-31 23:04:58 +0300

    Create serial_test.asm

---
+++ pic16f877a/serial_test.asm
@@ -0,0 +1,47 @@
+/*
+ * File:   main.c
+ * Author: prems
+ *
+ * 
+ * 
+ * 
+ * Created on 31 A?ustos 2023 Per?embe, 20:33
+ */
+
+#pragma config FOSC     = HS
+#pragma config WDTE     = OFF
+#pragma config PWRTE    = OFF
+#pragma config CP       = OFF
+
+#include <xc.h>
+
+
+
+void serialBegin(){
+    TRISC = 0b10000000;
+    TXSTA = 0b00100100;
+    RCSTA = 0b10010000;
+    SPBRG = 25;
+    TXREG = 0x00;
+}
+
+void serialWriteByte(char data){
+    while(!TRMT);
+    TXREG = data;
+}
+
+void serialPrint(char *str){
+    for(int i=0; str[i]!='\0'; i++){
+        serialWriteByte(str[i]);
+    }
+}
+
+void main(void) {
+    serialBegin();
+    for(;;){
+        serialPrint("Selam dunya\r\n");
+        RC0 = ~RC0;
+        __delay_ms(1000);
+    }
+    return;
+}
